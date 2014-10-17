0bd7f3ed886732fef437c23f36f6b112d45d6917
dfkennedy <dfkennedy@mac.com>
2014-10-17 00:46:13 -0500

    first rev of project

--- postcard/ViewController.swift
+++ postcard/ViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var enterNameText: UITextField!
+    @IBOutlet weak var enterMessageTextfield: UITextField!
+    @IBOutlet weak var mailButton: UIButton!
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +25,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sendMailButtonPressed(sender: UIButton) {
+        //anything in these brackets will execute when button is pressed
+        messageLabel.hidden = false
+        //variable.attribute = state/declaration
+        messageLabel.text = enterMessageTextfield.text
+        //always declare the item you want to effect first
+        //for example, in the above, I want to edit messageLabel.text so I put it first
+        messageLabel.textColor = UIColor.redColor()
+        //changes color.
+        enterMessageTextfield.text = ""
+        //clears text field
+        enterMessageTextfield.resignFirstResponder()
+        //hides the keyboard after you hit the button. () tells you it's a function
+        mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
+        //changes text on button
+    }
 
 }
 
